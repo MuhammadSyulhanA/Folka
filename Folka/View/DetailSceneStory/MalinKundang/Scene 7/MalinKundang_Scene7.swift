24285582648ae7c4d3e83d0e21a05ecac9a3a39e
ferryjulian <ferryjulian.fj@gmail.com>
2022-11-23 12:48:28 +0700

    revisi asset scene 7 dan add scene 7

--- Folka/View/DetailSceneStory/MalinKundang/Scene 7/MalinKundang_Scene7.swift
+++ Folka/View/DetailSceneStory/MalinKundang/Scene 7/MalinKundang_Scene7.swift
@@ -10,4 +10,15 @@ import GameplayKit
 
 class MalinKundang_Scene7: SKScene {
     
+    let fieldBackground = SKSpriteNode(imageNamed: "backgroundField")
+    
+    let buttonNext = SKSpriteNode(imageNamed: "buttonNext")
+    var buttonNextAction: SKAction?
+    
+    let buttonPrevious = SKSpriteNode(imageNamed: "buttonPrevious")
+    var buttonPreviousAction: SKAction?
+    
+    let textLayout = SKSpriteNode(imageNamed: "textLayout")
+    var textStory = SKLabelNode(fontNamed: "McLaren")
+
 }
