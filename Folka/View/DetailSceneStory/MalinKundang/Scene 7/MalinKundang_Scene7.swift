82a3e062547e5d041eb0f1e0d80dfd8bca8a62c0
ferryjulian <ferryjulian.fj@gmail.com>
2022-11-23 12:55:29 +0700

    revisi asset scene 7 and add scene 7

--- Folka/View/DetailSceneStory/MalinKundang/Scene 7/MalinKundang_Scene7.swift
+++ Folka/View/DetailSceneStory/MalinKundang/Scene 7/MalinKundang_Scene7.swift
@@ -12,6 +12,13 @@ class MalinKundang_Scene7: SKScene {
     
     let fieldBackground = SKSpriteNode(imageNamed: "backgroundField")
     
+    let nonCharacterHome = SKSpriteNode(imageNamed: "homeMalin1")
+    var animationHome: SKAction?
+    
+    let nonCharacterBabyChick = SKSpriteNode(imageNamed: "babyChicken")
+    
+    let nonCharacterMomChick = SKSpriteNode(imageNamed: "mommyChicken")
+    
     let buttonNext = SKSpriteNode(imageNamed: "buttonNext")
     var buttonNextAction: SKAction?
     
