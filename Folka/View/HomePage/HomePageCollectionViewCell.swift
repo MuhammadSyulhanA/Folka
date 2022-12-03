bcd506c815c4df161e1891364fbd2c8e0e58561c
putrird11 <putrirahmadewi77@gmail.com>
2022-12-03 21:59:39 +0700

    Add Home Page View Controller

---
+++ Folka/View/HomePage/HomePageCollectionViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  HomePageCollectionViewCell.swift
+//  Folka
+//
+//  Created by PUTRI RAHMADEWI on 03/12/22.
+//
+
+import UIKit
+
+class HomePageCollectionViewCell: UICollectionViewCell {
+//  @IBOutlet weak var image: UIImageView!
+//  @IBOutlet weak var titleCerita: UILabel!
+  
+  @IBOutlet weak var titleCerita: UILabel!
+  @IBOutlet weak var image: UIImageView!
+  static let identifier = "HomePageCollectionViewCell"
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+  
+  func configureCell(HomePage: HomePage){
+    self.image.image = UIImage(named: HomePage.imageCover)
+    self.titleCerita.text = HomePage.title
+    image.layer.cornerRadius = 8
+  }
+}
